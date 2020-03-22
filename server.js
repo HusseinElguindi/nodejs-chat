2fe867335bfb3812e81f58c156e208a3fd5231ee
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-03-22 17:10:29 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+var myArgs = process.argv.slice(2);
+var port = parseInt(myArgs[0]);
+
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', function connection(ws) {
+    console.log("Connected");
+
     ws.on('message', function incoming(message) {
         ws.send(message);
     });
