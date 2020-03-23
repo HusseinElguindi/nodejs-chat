dc102be84c5c212b27285fa0ebf25da9885f5b21
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-03-23 18:24:24 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -5,12 +5,46 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: port });
 
+var clients = [];
+
 wss.on('connection', function connection(ws) {
-    console.log("Connected");
+
+    clients.push(ws);
+    console.log(clients.length);
 
     ws.on('message', function incoming(message) {
-        ws.send(message);
+        var obj = JSON.parse(message);
+
+        if (obj.type == "name_change") {
+
+        }
+        else if (obj.type == "message") {
+            wss.broadcastMsg(message);
+        }
+        else if (obj.type == "command") {
+            if (obj.data == "./connections") {
+                let obj = {
+                    type: "message",
+                    name: "server",
+                    data: `There are ${clients.length} connections.`
+                };
+
+                ws.send(JSON.stringify(obj));
+            }
+        }
+
     });
 
-    // ws.send('something');
+    ws.on('close', function close(reasonCode, description) {
+        clients.splice(clients.indexOf(ws), 1);
+        console.log(clients.length);
+    });
+
 });
+
+wss.broadcastMsg = function (msg) {
+    console.log(msg);
+    clients.forEach(function each(client) {
+        client.send(msg);
+     });
+ };
