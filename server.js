5586d6d616978dd3089ac2e4ec845d325bd5376d
SHRKV2 <34726859+SHRKV2@users.noreply.github.com>
2020-03-25 00:03:10 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -6,61 +6,76 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: port });
 
 var clients = [];
+startServer();
 
-wss.on('connection', function connection(ws) {
+function startServer() 
+{
+    wss.on('connection', function connection(ws) {
 
-    clients.push(ws);
-    console.log(clients.length);
-
-    ws.on('message', function incoming(message) {
-        var obj = JSON.parse(message);
-
-        if (obj.type == "name_change") {
-
-        }
-        else if (obj.type == "message") {
-            wss.broadcastMsg(message);
-        }
-        else if (obj.type == "command") {
-            if (obj.data == "./connections") {
-
-                let obj = {
-                    type: "message",
-                    usr: {
-                        name: "Server",
-                        id: -1,
-                        color: "rgb(0, 0, 0)"
-                    },
-                    data: `There are ${clients.length} connections.`
-                };
-
-                ws.send(JSON.stringify(obj));
-            }
-        }
-    });
-
-    ws.on('close', function close(reasonCode, description) {
-        clients.splice(clients.indexOf(ws), 1);
+        clients.push(ws);
         // console.log(clients.length);
 
-        let obj = {
-            type: "message",
-            usr: {
-                name: "Server",
-                id: -1,
-                color: "rgb(0, 0, 0)"
-            },
-            data: "User left."
-        };
+        ws.on('message', function incoming(message) {
+            
+            console.log(message);
+            
+            if (message == "ping")
+            {
+                return;
+            }
 
-        ws.send(JSON.stringify(obj));
+            var obj = JSON.parse(message);
+
+            // if (obj.type == "name_change") {
+
+            // }
+            if (obj.type == "message") {
+                wss.broadcastMsg(message);
+            }
+            else if (obj.type == "command") {
+                if (obj.data == "./connections") {
+
+                    let obj = {
+                        type: "message",
+                        usr: {
+                            name: "Server",
+                            id: -1,
+                            color: "rgb(0, 0, 0)"
+                        },
+                        data: `There are ${clients.length} connections.`
+                    };
+
+                    ws.send(JSON.stringify(obj));
+                }
+            }
+        });
+
+        ws.on('close', function close(reasonCode, description) {
+
+            console.log("left");
+            clients.splice(clients.indexOf(ws), 1);
+            // console.log(clients.length);
+
+            let obj = {
+                type: "message",
+                usr: {
+                    name: "Server",
+                    id: -1,
+                    color: "rgb(0, 0, 0)"
+                },
+                data: "User has left."
+            };
+
+            wss.broadcastMsg(JSON.stringify(obj));
+        });
+
     });
 
-});
+    wss.broadcastMsg = function (msg) {
+        console.log(msg);
+        clients.forEach(function each(client) {
+            client.send(msg);
+        });
+    };
 
-wss.broadcastMsg = function (msg) {
-    console.log(msg);
-    clients.forEach(function each(client) {
-        client.send(msg);
-     });
- };
+}
